6e55ee535a1f473a4a3da3c925e4dd1f2759ae8f
Keith Naber <knaber4124@gmail.com>
2019-07-10 00:08:37 -0400

    added notations and updated image

--- assets/game.js
+++ assets/game.js
@@ -1,71 +1,72 @@
-//Create an array of words
-const choices=["court","steal","teams","layup","block","dunk"];
+//Establish Word Choices
+const choices = ["court", "steal", "teams", "layup", "block", "dunk"];
 
-//Choose word randomly
-let randNumber= Math.floor(Math.random() * choices.length);
-let chosenWord= choices[randNumber];
-let rightGuess=[];
-let wrongGuess=[];
-let underscore=[];
+//setting up base let statements
+let randNumber = Math.floor(Math.random() * choices.length);
+let chosenWord = choices[randNumber];
+let rightGuess = [];
+let wrongGuess = [];
+let underscore = [];
 
 console.log(chosenWord);
 
-//Create underscores based on length of word
+//generate underscores in console
 let genUnderscore = () => {
-    for(let i=0; i < chosenWord.length; i++){
+    for (let i = 0; i < chosenWord.length; i++) {
         underscore.push('_');
-        
     }
     return underscore;
 }
 console.log(genUnderscore());
 
+//allow player to enter guesses into log and determine if correct or not
 document.addEventListener("keypress", (event) => {
     let keyword = String.fromCharCode(event.keyCode);
     console.log(chosenWord.indexOf(keyword));
-    if(chosenWord.indexOf(keyword)>-1 ){
+    if (chosenWord.indexOf(keyword) > -1) {
         console.log(true);
         rightGuess.push(keyword);
         console.log(rightGuess);
     }
-    else{
-        console.log(false);
+    else {
+        
         wrongGuess.push(keyword);
         console.log(wrongGuess);
     }
     underscore[chosenWord.indexOf(keyword)] = keyword;
-        console.log(underscore);
+    console.log(underscore);
 
-    
 
-    
 
-    
 
-    
-    
+
+
+
+
+
 })
+// 
+function changeAnswers () {
+    let answers = document.getElementById('answers');
+    answers.innerHTML = 'WORKING';
+}
+window.onload = changeAnswers;
 
+//function answerWrong () {
+    //let wrongWord = document.getElementById('wrongWord');
+    //wrongWord.innerHTML = wrongGuess;
+//}
+//window.onload = answerWrong;
 
-if(underscore===chosenWord){
-        alert=('YOU WIN!');
-    }
-
-
-
-function refreshPage(){
+//button functionality to restart game
+function refreshPage() {
     window.location.reload();
-} 
-
-        
-    
-
-    
+}
 
 
-//User guesses letter
 
 
-//If right, reveal letter in underscores
 
-//If wrong, display in wrong guesses
+
+
+
