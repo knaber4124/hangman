bd44dc57ecffe545c1b51585afbef921ced94ae9
Keith Naber <knaber4124@gmail.com>
2019-07-12 22:44:13 -0400

    establish loss trigger,decrease guesses when wrong

--- assets/game.js
+++ assets/game.js
@@ -7,13 +7,26 @@ let chosenWord = choices[randNumber];
 let rightGuesses = [];
 let wrongGuesses = [];
 let underscore = [];
-let underscoreJoined=[];
-let guessesRemain=10;
-let wins=0;
-let losses=0;
+let underscoreJoined = [];
+let guessesRemain = 10;
+let wins = 0;
+let losses = 0;
 
 console.log(chosenWord);
 console.log(guessesRemain);
+
+//DOM manipulation
+$('.winResult').append(wins);
+$('.lossResult').append(losses);
+$('.guessRemain').append(guessesRemain);
+$('.wrongWord').append(wrongGuesses);
+
+if (guessesRemain === 0) {
+    alert('You Lose');
+};
+
+
+
 //generate underscores
 let genUnderscore = () => {
     for (let i = 0; i < chosenWord.length; i++) {
@@ -23,31 +36,42 @@ let genUnderscore = () => {
 }
 console.log(genUnderscore());
 
-//allow player to enter guesses into log and determine if correct or not
+//allow player to enter guesses and determine if correct or not
 document.addEventListener("keypress", (event) => {
     let keyword = String.fromCharCode(event.keyCode);
     console.log(chosenWord.indexOf(keyword));
     if (chosenWord.indexOf(keyword) > -1) {
         console.log(true);
-        rightGuesses.push(keyword);
-        console.log('rightGuesses '+rightGuesses);
+        $('#answers').append(keyword);
+        console.log('rightGuesses ' + rightGuesses);
     }
     else {
 
-        wrongGuesses.push(keyword);
+        $('.wrongWord').append(keyword);
         console.log('wrongGuesses ' + wrongGuesses);
-        guessesRemain--;
+        guessesRemain --;
+        $('.guessRemain').text(guessesRemain);
+        if (guessesRemain === 0) {
+            alert('You Lose');
+            losses ++;
+            window.location.reload(randNumber);
+            let guessesRemain = 10;
+            let wrongGuesses = [];
+        };
     }
     underscore[chosenWord.indexOf(keyword)] = keyword;
-    console.log('underscore '+underscore);
+    console.log('underscore ' + underscore);
 
 
 
 
 })
+
+
+
 let underscoreJoin = underscore.join(' ');
- console.log(underscoreJoin);
- underscoreJoined.push(underscoreJoin);
+console.log(underscoreJoin);
+underscoreJoined.push(underscoreJoin);
 
 
 function changeAnswers() {
@@ -55,33 +79,30 @@ function changeAnswers() {
     answers.innerHTML = underscoreJoin;
 };
 window.onload = changeAnswers;
+$('#answers').append(changeAnswers);
 
 // use push on rightAnswer into underscoreJoin
 // change button to rerun randNumber
 // usejQuery append to change selected word guesses
 
-//function answerWrong () {
-//let wrongWord = document.getElementById('wrongWord');
-//wrongWord.innerHTML = wrongGuesses;
 
-//}
-//window.onload = answerWrong;
 
-//button functionality to restart game
-let underscoreString= rightGuesses.toString();
 
-console.log('underscoreString '+underscoreString);
+let underscoreString = rightGuesses.toString();
 
-if (underscoreString===chosenWord){
+console.log('underscoreString ' + underscoreString);
+//Win Alert
+if (underscoreString === chosenWord) {
     alert('YOU WIN!');
 };
-if (guessesRemain===0){
-    alert('You Lose');
-};
+//Loss Alert
 
+// button tto generate new game
 function refreshPage() {
     window.location.reload(randNumber);
-}
+    let guessesRemain = 10;
+    let wrongGuesses = [];
+};
 
 
 
